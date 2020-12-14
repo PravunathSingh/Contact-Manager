8b75d3eaac701feaed3d7c5de64f9195ef143b7d
Pravunath Singh <pravunathsingh@gmail.com>
2020-12-14 15:16:59 +0530

    Updated the App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
-import Contact from './components/Contact';
-import Header from './components/Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Contacts from './components/contacts/Contacts';
+import Header from './components/layout/Header';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+import AddContact from './components/contacts/AddContact';
+import EditContact from './components/contacts/EditContact';
+import Provider from './context';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -8,15 +14,23 @@ import './App.css';
 class App extends Component {
   render() {
     // can add any amount of JS
-
     return (
-      <div className="App">
-        <Header branding="Contact Manager" />
-        <div className="container">
-          <Contact name="Pravunath Singh" email="pravunathsingh@gmail.com" phone="+918777655512" />
-          <Contact name="Raghunath Singh" email="raghunathsingh@gmail.com" phone="+919836882964" />
-        </div>
-      </div>
+      <Provider>
+        <Router>
+          <div className="App">
+            <Header branding="My Contacts" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contacts/add" component={AddContact} />
+                <Route exact path="/contacts/edit/:id" component={EditContact} />
+                <Route exact path="/about/" component={About} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
